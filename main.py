8000e7771a400ecfd4e0d2ed189eeccb5879bae4
Torbjørn Sitre <torbjorn.sitre@gmail.com>
2022-12-20 20:34:05 +0100

    Added DB commands

--- main.py
+++ main.py
@@ -1,13 +1,77 @@
 """Importing the needed modules"""
 from flask import Flask, flash, render_template, request, session, redirect
 from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect(path)
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+connection = create_connection("test.db")
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  age INTEGER,
+  gender TEXT,
+  nationality TEXT
+);
+"""
+
+execute_query(connection, create_users_table)
+
+create_users = """
+INSERT INTO
+  users (name, age, gender, nationality)
+VALUES
+  ('James', 25, 'male', 'USA'),
+  ('Leila', 32, 'female', 'France'),
+  ('Brigitte', 35, 'female', 'England'),
+  ('Mike', 40, 'male', 'Denmark'),
+  ('Elizabeth', 21, 'female', 'Canada');
+"""
+
+execute_query(connection, create_users)
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The erros '{e}' occurred")
+
+select_users = "SELECT * from users"
+users = execute_read_query(connection, select_users)
+
+for user in users:
+    print(user)
+
 
 db = SQLAlchemy()
 
 app = Flask(__name__)
 
 # configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 # initialize the app with the extension
 db.init_app(app)
 
@@ -26,6 +90,8 @@ def homepage():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    # rows = db.execute("SELECT * FROM tbl1")
+    # print(rows)
     if 'username' in session:
         print("Your are logged in as ", session["username"])
         return
